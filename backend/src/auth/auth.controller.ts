3066f11a55471a175a6613cf31edcadacfc46e45
raigrc <ravenjstn.grc@gmail.com>
2024-12-24 12:15:26 +0800

    add jwt feature with passport

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import User from "../users/users.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(404).json({ message: "Invalid email or password" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user?.password!);
+    if (!passwordMatch) {
+      res.status(404).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { _id: user?._id },
+      process.env.JWT_SECRET || "default_secret",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token, message: "Login successful!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error: error });
+  }
+};
