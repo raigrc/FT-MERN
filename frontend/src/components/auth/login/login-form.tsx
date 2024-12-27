65da0aca905e09718c9858bf2d7d42f5a153ae00
raigrc <ravenjstn.grc@gmail.com>
2024-12-27 18:26:20 +0800

    update to react hook form

--- frontend/src/components/auth/login/login-form.tsx
+++ frontend/src/components/auth/login/login-form.tsx
@@ -1,6 +1,5 @@
 import { Button } from "../../ui/button";
 import { Input } from "../../ui/input";
-import { Label } from "../../ui/label";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema, LoginSchemaType } from "@/schema/LoginSchema";
@@ -9,16 +8,19 @@ import { useState, useTransition } from "react";
 import { axiosLogin } from "@/api/axios.login";
 import { useUserStore } from "@/store/useUserStore";
 import { useNavigate } from "react-router-dom";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const LoginForm = () => {
   const { setUser } = useUserStore();
   const [error, setError] = useState("");
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    resetField,
-  } = useForm<LoginSchemaType>({ resolver: zodResolver(LoginSchema) });
+  const form = useForm<LoginSchemaType>({ resolver: zodResolver(LoginSchema) });
 
   const [isPending, startTransition] = useTransition();
   const navigate = useNavigate();
@@ -28,7 +30,7 @@ const LoginForm = () => {
       axiosLogin(values).then((response) => {
         if (!response?.data || response.status !== 200) {
           setError(response?.data.message);
-          resetField("password");
+          form.resetField("password");
         } else {
           setUser(response.data.user);
           navigate("/dashboard");
@@ -38,37 +40,76 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-      <div className="space-y-4">
-        <div className="space-y-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            id="email"
-            {...register("email")}
-            disabled={isPending}
-          />
-          {errors.email && <ErrorMessage message={errors.email.message!} />}
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            id="password"
-            {...register("password")}
-            disabled={isPending}
-          />
-          {errors.password && (
-            <ErrorMessage message={errors.password.message!} />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  id="email"
+                  {...field}
+                  disabled={isPending}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
           )}
-        </div>
-      </div>
+        />
 
-      <Button className="w-full" type="submit" disabled={isPending}>
-        {isPending ? "Loading..." : "Login"}
-      </Button>
-      <ErrorMessage message={error} />
-    </form>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  id="password"
+                  {...field}
+                  disabled={isPending}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              {...register("email")}
+              disabled={isPending}
+            />
+            {errors.email && <ErrorMessage message={errors.email.message!} />}
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              {...register("password")}
+              disabled={isPending}
+            />
+            {errors.password && (
+              <ErrorMessage message={errors.password.message!} />
+            )}
+          </div>
+        </div> */}
+
+        <Button className="w-full" type="submit" disabled={isPending}>
+          {isPending ? "Loading..." : "Login"}
+        </Button>
+        <ErrorMessage message={error} />
+      </form>
+    </Form>
   );
 };
 
