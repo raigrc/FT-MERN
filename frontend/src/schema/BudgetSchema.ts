77bc6464e6f4fbc9055b8c194fdde9a37fba68a1
raigrc <ravenjstn.grc@gmail.com>
2024-12-28 22:44:52 +0800

    added start and end date function

--- frontend/src/schema/BudgetSchema.ts
+++ frontend/src/schema/BudgetSchema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const BudgetSchema = z.object({
-  categoryId: z.string(),
+  categoryId: z.string({ message: "Please select a category" }),
   amount: z.coerce.number({ message: "Amount must be number!" }),
-  start_date: z.date().optional(),
-  end_date: z.date().optional(),
+  start_date: z.date({ message: "Start date required" }),
+  end_date: z.date({ message: "End date required" }),
 });
 
 export type BudgetSchemaType = z.infer<typeof BudgetSchema>;
