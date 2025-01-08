ac919f82a674f5fb48d080e92d6058a7d73e6223
raigrc <ravenjstn.grc@gmail.com>
2025-01-08 19:44:33 +0800

    test crud functions on users

--- backend/src/users/__tests__/users.controller.test.ts
+++ backend/src/users/__tests__/users.controller.test.ts
@@ -1,22 +1,60 @@
 import supertest from "supertest";
 import app from "../../app";
 import User from "../users.model";
-import passport, { authenticate } from "passport";
-import { NextFunction, Request, Response } from "express";
 
 // jest.mock("../users.model.ts");
 
 const api = supertest(app);
 
-describe("CRUD Functions", () => {
-  describe("POST /users", () => {
-    it("should create a new user", async () => {
-      const mockUser = {
-        name: "test",
-        email: "test@gmail.com",
-        password: "hashedPassword",
-      };
+jest.mock("passport", () => ({
+  initialize: jest.fn(() => (req: any, res: any, next: any) => next()),
+  use: jest.fn(),
+  authenticate: () => (req: any, res: any, next: any) => {
+    req.user = {
+      _id: "1",
+    }; // Mock user data to bypass authentication
+    next();
+  },
+}));
 
+// jest.mock("passport-jwt", () => ({
+//   ExtractJwt: {
+//     fromExtractors: jest.fn().mockReturnValue((req: any) => req.cookies.token),
+//   },
+//   Strategy: jest.fn(),
+// }));
+
+describe("CRUD Functions", () => {
+  // const token = jwt.sign(
+  //   { _id: "60f5c0f3d5f3c2408f1b4d91" },
+  //   process.env.JWT_SECRET!,
+  //   {
+  //     expiresIn: "1h",
+  //   }
+  // );
+
+  const mockUsers = [
+    {
+      _id: "1",
+      name: "test1",
+      email: "test1@gmail.com",
+      password: "hashedPassword1",
+    },
+    {
+      _id: "2",
+      name: "test2",
+      email: "test2@gmail.com",
+      password: "hashedPassword2",
+    },
+  ];
+
+  describe("POST /users", () => {
+    const mockUser = {
+      name: "test",
+      email: "test@gmail.com",
+      password: "hashedPassword",
+    };
+    it("should create a new user", async () => {
       User.findOne = jest.fn().mockResolvedValue(null);
       User.prototype.save = jest.fn().mockResolvedValue(mockUser);
 
@@ -28,12 +66,6 @@ describe("CRUD Functions", () => {
     });
 
     it("should return 400 if user already exists", async () => {
-      const mockUser = {
-        name: "test",
-        email: "test@gmail.com",
-        password: "hashedPassword",
-      };
-
       User.findOne = jest.fn().mockResolvedValue(mockUser);
 
       const response = await api.post("/api/users").send(mockUser);
@@ -43,34 +75,69 @@ describe("CRUD Functions", () => {
     });
   });
 
-  // describe("GET /users", () => {
-  //   it("should get all users", async () => {
-  //     const mockUsers = [
-  //       {
-  //         _id: 1,
-  //         name: "test1",
-  //         email: "test1@gmail.com",
-  //         password: "hashedPassword1",
-  //       },
-  //       {
-  //         _id: 2,
-  //         name: "test2",
-  //         email: "test2@gmail.com",
-  //         password: "hashedPassword2",
-  //       },
-  //     ];
-  //     User.find = jest.fn().mockResolvedValue(mockUsers);
-  //     const response = await api
-  //       .get("/api/users")
-  //       .set("Cookie", "token=validToken");
+  describe("GET /users", () => {
+    it("should get all users", async () => {
+      User.find = jest.fn().mockResolvedValue(mockUsers);
 
-  //     console.log(response.body);
+      const response = await api.get("/api/users");
 
-  //     expect(response.status).toBe(200);
-  //     expect(response.body).toBeInstanceOf(Array);
-  //     expect(response.body.length).toBe(2);
-  //     expect(response.body[0]).toHaveProperty("name", "test1");
-  //     expect(response.body[1]).toHaveProperty("name", "test2");
-  //   });
-  // });
+      expect(response.status).toBe(200);
+      expect(response.body).toBeInstanceOf(Array);
+      expect(response.body.length).toBe(2);
+      expect(response.body[0]).toHaveProperty("name", "test1");
+      expect(response.body[1]).toHaveProperty("name", "test2");
+    });
+
+    it("should get one user", async () => {
+      User.findById = jest.fn().mockResolvedValue(mockUsers[0]);
+
+      const response = await api.get("/api/users/1");
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("name", "test1");
+      expect(response.body).toHaveProperty("email", "test1@gmail.com");
+    });
+
+    it("should get 404 or no users found", async () => {
+      User.findById = jest.fn().mockResolvedValue(null);
+
+      const response = await api.get(`/api/users/1`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toHaveProperty("message", "User not found!");
+    });
+  });
+
+  describe("PUT /users", () => {
+    const updatedUser = {
+      _id: "1",
+      name: "test3",
+      email: "test3@gmail.com",
+      password: "hashedPassword3",
+    };
+    it("should update user's data", async () => {
+      User.findByIdAndUpdate = jest
+        .fn()
+        .mockResolvedValue({ ...mockUsers[0], ...updatedUser });
+
+      const response = await api.put("/api/users/1").send(updatedUser);
+      console.log(response.body);
+
+      expect(response.status).toBe(200);
+      expect(response.body.user).toHaveProperty("name", "test3");
+      expect(response.body.user).toHaveProperty("email", "test3@gmail.com");
+      expect(response.body.message).toBe("User updated successfully!");
+    });
+  });
+
+  describe("DELETE /users", () => {
+    it("should delete existing user", async () => {
+      User.findByIdAndDelete = jest.fn().mockResolvedValue(mockUsers[0]);
+
+      const response = await api.delete("/api/users/1");
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe("Successfully deleted user!");
+    });
+  });
 });
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -47,7 +47,10 @@ export const findOneUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     const user: IUser | null = await User.findById(id);
 
-    if (!user) res.status(404).json({ message: "User not found!" });
+    if (!user) {
+      res.status(404).json({ message: "User not found!" });
+      return;
+    }
 
     res.status(200).json(user);
   } catch (error) {
@@ -67,7 +70,7 @@ export const updateUser = async (req: Request, res: Response) => {
 
     if (!user) res.status(404).json({ message: "User not found!" });
 
-    res.status(200).json(user);
+    res.status(200).json({ user, message: "User updated successfully!" });
   } catch (error) {
     res.status(500).json({
       error: error instanceof Error ? error.message : "Unknown error",
