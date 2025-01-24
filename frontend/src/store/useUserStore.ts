a58a52d8c693a57b2d22b7464523161e589b1132
raigrc <ravenjstn.grc@gmail.com>
2025-01-24 18:15:38 +0800

    change interface name from IUser to IUserStore

--- frontend/src/store/useUserStore.ts
+++ frontend/src/store/useUserStore.ts
@@ -1,7 +1,7 @@
 import { UserProps } from "@/types/user.types";
 import { create } from "zustand";
 
-interface IUser {
+interface IUserStore {
   user: UserProps | null;
   setUser: (user: UserProps) => void;
   clearUser: () => void;
@@ -9,7 +9,7 @@ interface IUser {
   loading: boolean;
   setLoading: (loading: boolean) => void;
 }
-export const useUserStore = create<IUser>((set) => ({
+export const useUserStore = create<IUserStore>((set) => ({
   user: null,
   setUser: (user) => set({ user, loading: false }),
   clearUser: () => set({ user: null, loading: false }),
