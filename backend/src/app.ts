1868b109622ab930a116453baf1e1691d7f7b0cc
raigrc <ravenjstn.grc@gmail.com>
2025-01-28 10:32:56 +0800

    change url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ft-mern-zeta.vercel.app/",
     credentials: true,
   })
 );
