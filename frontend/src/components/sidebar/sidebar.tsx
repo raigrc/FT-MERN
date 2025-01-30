9388a33d166d0d74439e7eba81846222ab4fae17
raigrc <ravenjstn.grc@gmail.com>
2025-01-30 23:04:28 +0800

    responsiveness for dashboard

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -7,40 +7,89 @@ import { Button } from "../ui/button";
 import { signOut } from "@/api/axios.signOut";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
+import { useTransition } from "react";
+import LoadingState from "../shared/loading";
+import useScreenWidth from "@/hooks/useScreenWidth";
+import NavigationMeidum from "./navigation-medium";
+import NavigationSmall from "./navigation-small";
 
 const Sidebar = () => {
   const { user, clearUser } = useUserStore();
+  const screenWidth = useScreenWidth();
+  const [isPending, startTransition] = useTransition();
   const navigate = useNavigate();
-  return (
-    <div className="min-w-[250px] space-y-6 overflow-auto border-r-2 px-2 py-6">
-      <SidebarHeader name={user?.name} email={user?.email} />
-      <div className="space-y-1">
-        {items.map((item) => (
-          <SidebarItem
-            key={item.name}
-            href={item.href}
-            name={item.name}
-            icon={<item.icon size={16} />}
-          />
-        ))}
+
+  const handleSignout = () => {
+    startTransition(() => {
+      signOut().then((response) => {
+        if (response.success) {
+          toast.info(response.message);
+          clearUser();
+          navigate("/login");
+        }
+      });
+    });
+  };
+  if (screenWidth >= 1024) {
+    return (
+      <div className="h-full min-w-[250px] space-y-6 overflow-auto border-r-2 px-2 py-6">
+        <SidebarHeader name={user?.name} email={user?.email} />
+        <div className="space-y-1">
+          {items.map((item) => (
+            <SidebarItem
+              key={item.name}
+              href={item.href}
+              name={item.name}
+              icon={<item.icon size={16} />}
+            />
+          ))}
+        </div>
+        <Separator />
+        <Button onClick={handleSignout} className="w-full" disabled={isPending}>
+          {isPending ? (
+            <>
+              <LoadingState>Signing out...</LoadingState>
+            </>
+          ) : (
+            "Sign out"
+          )}
+        </Button>
       </div>
-      <Separator />
-      <Button
-        onClick={() => {
-          signOut().then((response) => {
-            if (response.success) {
-              toast.info(response.message);
-              clearUser();
-              navigate("/login");
-            }
-          });
-        }}
-        className="w-full"
+    );
+  } else if (screenWidth >= 768) {
+    return <NavigationMeidum />;
+  } else {
+    return (
+      <NavigationSmall
+        title={<SidebarHeader name={user?.name} email={user?.email} />}
       >
-        Sign out
-      </Button>
-    </div>
-  );
+        <div className="space-y-1">
+          {items.map((item) => (
+            <SidebarItem
+              key={item.name}
+              href={item.href}
+              name={item.name}
+              icon={<item.icon size={16} />}
+            />
+          ))}
+          <Separator />
+          <Button
+            onClick={handleSignout}
+            className="w-full"
+            disabled={isPending}
+          >
+            {isPending ? (
+              <>
+                <LoadingState>Signing out...</LoadingState>
+              </>
+            ) : (
+              "Sign out"
+            )}
+          </Button>
+        </div>
+      </NavigationSmall>
+    );
+  }
 };
 
 export default Sidebar;
