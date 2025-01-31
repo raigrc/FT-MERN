1e913ff89864baa103c3d16f899d1b96dfe91455
raigrc <ravenjstn.grc@gmail.com>
2025-01-31 16:44:46 +0800

    change secure to true

--- backend/auth/auth.controller.ts
+++ backend/auth/auth.controller.ts
@@ -24,7 +24,7 @@ export const login = async (req: Request, res: Response): Promise<any> => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Ensure this is true in production
+      secure: true, // Ensure this is true in production
       maxAge: 3600000, // 1 hour
       sameSite: "none",
     });
